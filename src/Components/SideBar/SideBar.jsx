0f5f3c8b5103379fd0339287935a7bd01e8596e9
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-09-29 13:38:53 +0600

    sidebar working..

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import loc from '../../Img/locIcon.png';
+import profile from '../../Img/profile.png';
 import './SideBar.css';
 const SideBar = (props) => {
     // console.log("from:",props.time);
     const { time } = props;
-    console.log(time);
+    // console.log(time);
     let totalTime = 0;
     for (const t of time) {
         totalTime += t;
-        console.log(totalTime);
+        console.log("total:",totalTime);
     }
     return (
         <div>
+
+            <div className="profile">
+                <img src={profile} alt="" srcset="" />
+                <div className="names">
+                    <h4>Ashikur Rahman</h4>
+                    <small><img src={loc} alt="" srcset="" /> Ashulia,Savar,Dhaka</small>
+                </div>
+            </div>
             <h3>Exercise Details:</h3>
             <p>Exercise Time: {totalTime} Seconds</p>
 
