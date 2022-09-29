b64cda7e000c8c19be9a9648a554008ca5b52716
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-09-29 11:09:57 +0600

    json  loaded

--- src/Components/CLUB/Club.jsx
+++ src/Components/CLUB/Club.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+
+import React, { useEffect, useState } from 'react';
 import "./Club.css";
 const Club = () => {
+const [activities, setActivities] = useState([])
+
+useEffect(()=>{
+fetch('clubData.json').then(res=>res.json()).then(data=> setActivities(data))
+},[])
+
   return (
     <div>
-      <h2>Select your today's exercise!</h2>
+      <h2>Select your today's exercise! from {activities.length}</h2>
       <div className="club-container">
         <div className="exercises-container">This is excercises</div>
         <div className="sidebar-container">This is sidebar</div>
